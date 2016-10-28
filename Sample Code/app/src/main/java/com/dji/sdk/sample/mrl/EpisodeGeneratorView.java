81c084c78d7cbbb7fdc8a75b42d7a2cfa5b1efa3
pjhjohn <pjhjohn@gmail.com>
2016-10-28 23:40:31 +0900

    Initializer for Customized FlightController modes
    Configurations
    - CoordinateSystem : Body (of Aircraft)
    - Yaw : AngularVelocity
    - Roll, Pitch, Throttle : Velocity
    - Corresponding boundary values are stated in VirtualStickCommand
    Lifecycle
    - Set our flight controller config in onAttachToWindow
    - Restore flight controller config back in onDetachFromWindow

--- Sample Code/app/src/main/java/com/dji/sdk/sample/mrl/EpisodeGeneratorView.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/mrl/EpisodeGeneratorView.java
@@ -9,30 +9,26 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.dji.sdk.sample.R;
 import com.dji.sdk.sample.common.DJISampleApplication;
 import com.dji.sdk.sample.common.Utils;
-import com.dji.sdk.sample.mrl.network.api.Api;
 import com.dji.sdk.sample.mrl.network.model.Episode;
 import com.dji.sdk.sample.utils.DJIModuleVerificationUtil;
 
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import dji.common.flightcontroller.DJISimulatorInitializationData;
-import dji.thirdparty.retrofit2.Call;
-import dji.thirdparty.retrofit2.Callback;
-import dji.thirdparty.retrofit2.Response;
-import dji.thirdparty.rx.Observable;
-import dji.thirdparty.rx.android.schedulers.AndroidSchedulers;
+import dji.common.flightcontroller.DJIVirtualStickFlightCoordinateSystem;
+import dji.common.flightcontroller.DJIVirtualStickRollPitchControlMode;
+import dji.common.flightcontroller.DJIVirtualStickVerticalControlMode;
+import dji.common.flightcontroller.DJIVirtualStickYawControlMode;
+import dji.sdk.flightcontroller.DJIFlightController;
 import dji.thirdparty.rx.functions.Action1;
-import dji.thirdparty.rx.schedulers.Schedulers;
 import timber.log.Timber;
 
 public class EpisodeGeneratorView extends RelativeLayout {
@@ -71,9 +67,58 @@ public class EpisodeGeneratorView extends RelativeLayout {
     }
 
     @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        this.setControlMode();
+    }
+
+    @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
+        this.restoreOldControlMode();
         mUnbinder.unbind();
+    }
+
+    private DJIVirtualStickYawControlMode mPrevYawControlMode;
+    private DJIVirtualStickRollPitchControlMode mPrevRollPitchControlMode;
+    private DJIVirtualStickVerticalControlMode mPrevVerticalControlMode;
+    private DJIVirtualStickFlightCoordinateSystem mPrevFlightCoordinateSystem;
+    private void setControlMode() {
+        // Check FlightController Accessibility
+        if (DJISampleApplication.getAircraftInstance() == null || DJISampleApplication.getAircraftInstance().getFlightController() == null) return;
+        DJIFlightController controller = DJISampleApplication.getAircraftInstance().getFlightController();
+
+        // HorizontalFlightCoordinateSystem : Body by default
+        mPrevFlightCoordinateSystem = controller.getHorizontalCoordinateSystem();
+        controller.setHorizontalCoordinateSystem(DJIVirtualStickFlightCoordinateSystem.Body);
+        this.log2ListLogger("FlightCoordinateSystem : %s => %s", mPrevFlightCoordinateSystem.name(), controller.getHorizontalCoordinateSystem().name());
+
+        // YawControlMode : AngularVelocity (Palstance) by default
+        mPrevYawControlMode = controller.getYawControlMode();
+        controller.setYawControlMode(DJIVirtualStickYawControlMode.AngularVelocity);
+        this.log2ListLogger("YawControlMode : %s => %s", mPrevYawControlMode.name(), controller.getYawControlMode().name());
+
+        // RollPitchControlMode : Angle by default
+        mPrevRollPitchControlMode = controller.getRollPitchControlMode();
+        controller.setRollPitchControlMode(DJIVirtualStickRollPitchControlMode.Velocity);
+        this.log2ListLogger("RollPitchControlMode : %s => %s", mPrevRollPitchControlMode.name(), controller.getRollPitchControlMode().name());
+
+        // VerticalControlMode : Velocity by default
+        mPrevVerticalControlMode = controller.getVerticalControlMode();
+        controller.setVerticalControlMode(DJIVirtualStickVerticalControlMode.Velocity);
+        this.log2ListLogger("VerticalControlMode : %s => %s", mPrevVerticalControlMode.name(), controller.getVerticalControlMode().name());
+    }
+
+    private void restoreOldControlMode() {
+        // Check FlightController Accessibility
+        if (DJISampleApplication.getAircraftInstance() == null || DJISampleApplication.getAircraftInstance().getFlightController() == null) return;
+        DJIFlightController controller = DJISampleApplication.getAircraftInstance().getFlightController();
+
+        // Restore old control modes
+        controller.setHorizontalCoordinateSystem(mPrevFlightCoordinateSystem);
+        controller.setYawControlMode(mPrevYawControlMode);
+        controller.setRollPitchControlMode(mPrevRollPitchControlMode);
+        controller.setVerticalControlMode(mPrevVerticalControlMode);
     }
 
     private void initUI(Context context, AttributeSet attrs) {
@@ -177,6 +222,9 @@ public class EpisodeGeneratorView extends RelativeLayout {
     }
 
     /* Loggers */
+    private void log2ListLogger(String format, Object ...args) {
+        log2ListLogger(String.format(format, args));
+    }
     private void log2ListLogger(int resid) {
         log2ListLogger(this.getContext().getResources().getString(resid));
     }
