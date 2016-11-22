5cefda4b116c7fa3979460c5ffa7204b51617074
pjhjohn <pjhjohn@gmail.com>
2016-11-22 10:59:29 +0900

    Simulator Event emits x, y, z, rx, ry, rz format

--- Sample Code/app/src/main/java/com/dji/sdk/sample/mrl/network/model/SimulatorEvent.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/mrl/network/model/SimulatorEvent.java
@@ -3,29 +3,21 @@ package com.dji.sdk.sample.mrl.network.model;
 import dji.common.flightcontroller.DJISimulatorStateData;
 
 public class SimulatorEvent {
-    public Double latitude;
-    public Double longitude;
-    public Float yaw;
-    public Float pitch;
-    public Float roll;
-    public Float positionX;
-    public Float positionY;
-    public Float positionZ;
-    public Boolean isFlying;
-    public Boolean areMotorsOn;
-    public Long time;
+    public Float rx;
+    public Float ry;
+    public Float rz;
+    public Long t;
+    public Float x;
+    public Float y;
+    public Float z;
 
     public SimulatorEvent(DJISimulatorStateData simulatorStateData, long elapsedTime) {
-        this.latitude = simulatorStateData.getLatitude();
-        this.longitude = simulatorStateData.getLongitude();
-        this.yaw = simulatorStateData.getYaw();
-        this.pitch = simulatorStateData.getPitch();
-        this.roll = simulatorStateData.getRoll();
-        this.positionX = simulatorStateData.getPositionX();
-        this.positionY = simulatorStateData.getPositionY();
-        this.positionZ = simulatorStateData.getPositionZ();
-        this.isFlying = simulatorStateData.isFlying();
-        this.areMotorsOn = simulatorStateData.areMotorsOn();
-        this.time = elapsedTime;
+        this.rx = simulatorStateData.getRoll();
+        this.ry = -simulatorStateData.getPitch();
+        this.rz = -simulatorStateData.getYaw();
+        this.t = elapsedTime;
+        this.x = simulatorStateData.getPositionX();
+        this.y = simulatorStateData.getPositionY();
+        this.z = simulatorStateData.getPositionZ();
     }
 }
